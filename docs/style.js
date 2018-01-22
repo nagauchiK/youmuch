b276f2daa3a84dfa440a91352f924a8e64d3b5ed
Mr.kaito <mr_shigeshige@yahoo.co.jp>
2018-01-22 21:56:32 +0900

    Update style.js

--- docs/style.js
+++ docs/style.js
@@ -1,24 +1,4 @@
 function init() {
+	
 	alert("youmuch");
-	var data = "";
-	$(function() {
-			$.ajax({
-			type : 'GET',
-			cache : false,
-			url : '',
-        	data: data
-   		})  
-		.done(function(data) {
-			alert("success");
-    		$('.main').html("success");
-		})
-		.fail(function(XMLHttpRequest, textStatus, errorThrown) {
-			alert("error");
-		    console.log("XMLHttpRequest : " + XMLHttpRequest.status);
-        	console.log("textStatus     : " + textStatus);
-        	console.log("errorThrown    : " + errorThrown.message);
-		})
-		.always(function() {
-		});
-	});
 }
