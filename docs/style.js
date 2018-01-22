72094a20a605da96bc86f29e6fc0cbbb394f1a8f
Mr.kaito <mr_shigeshige@yahoo.co.jp>
2018-01-22 22:04:46 +0900

    Update style.js

--- docs/style.js
+++ docs/style.js
@@ -3,12 +3,7 @@ $(function () {
   $.ajax({
     url:'../tkn.dat',
     success: function(data){
-      var data_array = data.split(/\r\n|\r|\n/);  // 改行コードで分割
-      var len = data_array.length;
-
-      for (var i = 0; i < len; i++){
-        console.log(data_array[i]);
-      }
+      alert(data);
     }
   });
 });
