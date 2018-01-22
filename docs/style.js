fc07e9fea319e05203ade57ae5eaadf687e880e5
Mr.kaito <mr_shigeshige@yahoo.co.jp>
2018-01-22 22:32:39 +0900

    Update style.js

--- docs/style.js
+++ docs/style.js
@@ -2,14 +2,14 @@ function init() {
 $(function () {
   $.ajax({
     dataType: 'text',
-    url:'../tkn.dat'
+    url:'tkn.dat'
   })
   .done(function(data){
     alert(data);
   })
   .fail(function(XMLHttpRequest, textStatus, errorThrown) {
-		    alert( "error" ); //通信が失敗した場合、errorを表示
-		    console.log("XMLHttpRequest : " + XMLHttpRequest.status);
+		alert( "error" ); //通信が失敗した場合、errorを表示
+		console.log("XMLHttpRequest : " + XMLHttpRequest.status);
         	console.log("textStatus     : " + textStatus);
         	console.log("errorThrown    : " + errorThrown.message);
 	});
